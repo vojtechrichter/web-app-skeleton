a47e990a47144aecd0d2b2c736d8a86862bb427b
vojtechrichter <vojta3218@gmail.com>
2025-06-01 12:17:01 +0200

    feat: dialogs

--- app/Presentation/Home/HomePresenter.php
+++ app/Presentation/Home/HomePresenter.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace App\Presentation\Home;
 
 use App\Presentation\BasePresenter;
-use Nette\Bridges\ApplicationLatte\Template;
 
 final class HomePresenter extends BasePresenter
 {
